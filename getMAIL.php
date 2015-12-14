1e131ef8dcee7a5e8aca563f0e69a5aa68b9e066
LightsOnHudson <bshaver@officemini.home.lan>
2015-12-14 16:41:42 -0700

    Getting Messages, setting high water mark

--- getMAIL.php
+++ getMAIL.php
@@ -1,19 +1,114 @@
 #!/usr/bin/php
 <?php
 
-error_reporting(0);
+//error_reporting(0);
 
-//r connect to gmail */
+$pluginName ="SMS";
+$myPid = getmypid();
+
+$messageQueue_Plugin = "MessageQueue";
+$MESSAGE_QUEUE_PLUGIN_ENABLED=false;
+
+$DEBUG=false;
+
+$skipJSsettings = 1;
+include_once("/opt/fpp/www/config.php");
+include_once("/opt/fpp/www/common.php");
+include_once("functions.inc.php");
+include_once("commonFunctions.inc.php");
+include_once("profanity.inc.php");
+include_once ("GoogleVoice.php");
+
+$logFile = $settings['logDirectory']."/".$pluginName.".log";
+
+$messageQueuePluginPath = $pluginDirectory."/".$messageQueue_Plugin."/";
+
+$messageQueueFile = urldecode(ReadSettingFromFile("MESSAGE_FILE",$messageQueue_Plugin));
+
+if(file_exists($messageQueuePluginPath."functions.inc.php"))
+        {
+                include $messageQueuePluginPath."functions.inc.php";
+                $MESSAGE_QUEUE_PLUGIN_ENABLED=true;
+
+        } else {
+                logEntry("Message Queue Plugin not installed, some features will be disabled");
+        }
+
+require ("lock.helper.php");
+
+define('LOCK_DIR', '/tmp/');
+define('LOCK_SUFFIX', '.lock');
+
+$pluginConfigFile = $settings['configDirectory'] . "/plugin." .$pluginName;
+if (file_exists($pluginConfigFile))
+        $pluginSettings = parse_ini_file($pluginConfigFile);
+
+
+
+$EMAIL = urldecode($pluginSettings['EMAIL']);
+$PASSWORD = urldecode($pluginSettings['PASSWORD']);
+$PLAYLIST_NAME = urldecode($pluginSettings['PLAYLIST_NAME']);
+$WHITELIST_NUMBERS = urldecode($pluginSettings['WHITELIST_NUMBERS']);
+$CONTROL_NUMBERS = urldecode($pluginSettings['CONTROL_NUMBERS']);
+$REPLY_TEXT = urldecode($pluginSettings['REPLY_TEXT']);
+$VALID_COMMANDS = urldecode($pluginSettings['VALID_COMMANDS']);
+$MAIL_LAST_TIMESTAMP= urldecode($pluginSettings['MAIL_LAST_TIMESTAMP']);
+
+if(urldecode($pluginSettings['DEBUG'] != "")) {
+        $DEBUG=urldecode($pluginSettings['DEBUG']);
+}
+
+if($DEBUG)
+        print_r($pluginSettings);
+
+
+$COMMAND_ARRAY = explode(",",trim(strtoupper($VALID_COMMANDS)));
+$CONTROL_NUMBER_ARRAY = explode(",",$CONTROL_NUMBERS);
+
+
+$WHITELIST_NUMBER_ARRAY = explode(",",$WHITELIST_NUMBERS);
+
+$logFile = $settings['logDirectory']."/".$pluginName.".log";
+if($DEBUG)
+print_r($COMMAND_ARRAY);
+
+//give google voice time to sleep
+$GVSleepTime = 5;
+
+$ENABLED="";
+
+$ENABLED = trim(urldecode(ReadSettingFromFile("ENABLED",$pluginName)));
+
+
+
+if(($pid = lockHelper::lock()) === FALSE) {
+        exit(0);
+
+}
+
+if($ENABLED != "on" && $ENABLED != "1") {
+        logEntry("Plugin Status: DISABLED Please enable in Plugin Setup to use & Restart FPPD Daemon");
+        lockHelper::unlock();
+        exit(0);
+}
+
+if($DEBUG){
+        logEntry("user: ".$EMAIL);
+        logEntry("pass: ".$PASSWORD);
+}
+
+$gv = new GoogleVoice($EMAIL, $PASSWORD);
 
 //$hostname = '{imap.gmail.com:993/imap/ssl}ALL';
 $path="INBOX";
+
+$imap_search ='SUBJECT "SMS from"';
+
 $hostname = "{imap.gmail.com:993/imap/ssl/novalidate-cert}$path";
 //$hostname = '{imap.gmail.com:993/imap/ssl}INBOX';
-$username = 'brbshaver@gmail.com';
-include_once 'pass.php.inc';
 
 /* try to connect */
-$mbox = imap_open($hostname,$username,$password) or die('Cannot connect to Gmail: ' . imap_last_error());
+$mbox = imap_open($hostname,$EMAIL,$PASSWORD) or die('Cannot connect to Gmail: ' . imap_last_error());
 
 
 //Message body Format: 
@@ -25,7 +120,8 @@ $messageFormat = 1;
 	echo "total mesages: ".$totalrows."\n";
 
 	echo "retrieiving first 5 messages \n";
-        $emails = imap_search($mbox,'SUBJECT "SMS from"');
+        $emails = imap_search($mbox,$imap_search);
+        //$emails = imap_search($mbox,'SUBJECT "SMS from"');
         //$emails = imap_search($mbox,'ALL');
         //$emails = imap_search($mbox,'UNSEEN');
       //  $emails = imap_search($inbox,'ALL', SE_UID);
@@ -95,11 +191,20 @@ $i=0;
 		$message = substr($message,0,strpos($message,"\n"));
 		echo "Message: ".$message."\n";
 		echo "messagedate: ".$messageDate."\n";
-		$messageTimestamp = strtotime($messageDate);
-		echo "unix date: ".$messageTimestamp;
 
-		echo "message udate: ".$overview[0]->udate."\n";
-		echo "\n";
+		$messageTimestamp = $overview[0]->udate;
+
+		if($MAIL_LAST_TIMESTAMP < $messageTimestamp) {
+			logEntry("We have a new message");
+
+		} else {
+
+			logEntry("this message is not new");
+			continue;
+		}
+
+		logEntry("updating message last download to: ".$messageTimestamp);
+		WriteSettingToFile("MAIL_LAST_TIMESTAMP",$messageTimestamp,$pluginName);
 		echo "from: ".$from."\n";
 		$from =  $overview[0]->from;
 		echo "from: ".$from."\n";
@@ -112,6 +217,97 @@ $i=0;
 		echo "from: ".$from."\n";
 
 	 $status = imap_setflag_full($mbox, $mailUID, "\\Seen \\Flagged", ST_UID);
+ $MESSAGE_USED=false;
+        $messageText = $message;
+
+        logEntry("processing message: ".$mailUID." from: ".$from." Message: ".$messageText);
+
+                $messageText= preg_replace('/\s+/', ' ', $messageText);
+                $messageParts = explode(" ",$messageText);
+
+        if(in_array($from,$CONTROL_NUMBER_ARRAY))
+        {
+                ///message used is to make sure that we do not process a message twice if it is from a number that is both a whitelist AND control numbers
+                $MESSAGE_USED=true;
+                logEntry("Control number found: ".$from);
+                //process the command see if it is in the valid commands
+
+                //see if they sent in a playlist name???
+                //that would mean there is a space in the command.
+
+                //if(count($messageParts) > 1) {
+                //      logEntry("did we get a command with playlist");
+                //      logEntry("Command: ".$messageParts[0]);
+                //      logEntry("playlist: ".$messageParts[1]);
+                //}
+
+                if(in_array(trim(strtoupper($messageParts[0])),$COMMAND_ARRAY)) {
+                        logEntry("Command request: ".$messageText. " in uppercase is in control array");
+                        //do we have a playlist name?
+                        if($messageParts[1] != "") {
+
+                                processSMSCommand($from,$messageParts[0],$messageParts[1]);
+                        } else {
+
+                                //play the configured playlist@!!!! from the plugin
+                                processSMSCommand($from,$messageParts[0],$PLAYLIST_NAME);
+                        }
+
+                } else {
+                                //generic message to display from control number just like a regular user
+                                processSMSMessage($from,$messageText);
+                                $gv->sendSMS($from,$REPLY_TEXT);
+                                sleep(1);
+
+                                //processReadSentMessages();
+                        }
+
+                }
+
+ if(in_array($from,$WHITELIST_NUMBER_ARRAY) && !$MESSAGE_USED)
+
+                        {
+                                $MESSAGE_USED=true;
+                                logEntry($messageText. " is from a white listed number");
+                                processSMSMessage($from,$messageText);
+                                $gv->sendSMS($from,$REPLY_TEXT);
+                                //$gv->sendSMS($from,"Thank you for your message, it will be addedd to the queue: WHITELIST");
+                                sleep(1);
+                                //processReadSentMessages();
+
+        } else if(!$MESSAGE_USED){
+
+                                //not from a white listed or a control number so just a regular user
+                                //need to check for profanity
+                                //profanity checker API
+                                $profanityCheck = check_for_profanity($messageText);
+                                //$profanityCheck = profanityChecker($messageText);
+
+                                //if(!$profanityCheck) {
+                                //returns a list of array,
+                                if($profanityCheck['is-bad'] == 0 && $profanityCheck['bad-words-total'] == 0) {
+
+                                        logEntry("Message: ".$messageText. " PASSED");
+                                        $gv->sendSMS($from,$REPLY_TEXT);
+                                        //$gv->sendSMS($from,"Thank you for your message, it has been added to the queue");
+                                        processSMSMessage($from,$messageText);
+                                        sleep(1);
+                                 //       processReadSentMessages();
+
+                                } else {
+                                        logEntry("message: ".$messageText." FAILED");
+                                        $gv->sendSMS($from,"Your message contains profanity, sorry. More messages like these will ban your phone number");
+                                        sleep(1);
+                                  //      processReadSentMessages();
+
+                                }
+        }
+
+
+        }
+
+
+
 	
 	}
 
@@ -126,9 +322,10 @@ $i=0;
 
 
 
-}
+
 /* close the connection */
 imap_close($mbox);
+lockHelper::unlock();
 
 function get_string_between ($str,$from,$to) {
 
