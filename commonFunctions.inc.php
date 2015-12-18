5c8a3b345b2c0faeff5cda40951217cd3f57e922
LightsOnHudson <bshaver@officemini.home.lan>
2015-12-18 09:28:07 -0700

    Consolidating Functions

--- commonFunctions.inc.php
+++ commonFunctions.inc.php
@@ -1,5 +1,111 @@
 <?php
 
+//send response function
+function sendResponse($from,$REPLY_TEXT,$GMAIL_ADDRESS,$subject) {
+	global $DEBUG, $gv, $EMAIL, $RESPONSE_METHOD;
+
+	logEntry("Sending response using: ".$RESPONSE_METHOD);
+
+	switch ($RESPONSE_METHOD) {
+
+		case "SMS":
+			$gv->sendSMS($from,$REPLY_TEXT);
+				
+			break;
+				
+		case "EMAIL":
+			sendMail($GMAIL_ADDRESS, $EMAIL, $subject, $REPLY_TEXT);
+			break;
+	}
+
+
+
+}
+//sendmail using phpmailer function
+function sendMail($to, $from, $subject, $body) {
+	global $DEBUG, $EMAIL, $PASSWORD;
+
+	date_default_timezone_set('Etc/UTC');
+
+	if($DEBUG) {
+		echo "To: ".$to."\n";
+		echo "From: ".$from."\n";
+		echo "subject: ".$subject."\n";
+		echo "body: ".$body."\n";
+	}
+
+	//Create a new PHPMailer instance
+	$mail = new PHPMailer;
+
+	//Tell PHPMailer to use SMTP
+	$mail->isSMTP();
+
+	//Enable SMTP debugging
+	// 0 = off (for production use)
+	// 1 = client messages
+	// 2 = client and server messages
+	if($DEBUG) {
+		$mail->SMTPDebug = 2;
+	} else {
+
+		$mail->SMTPDebug = 0;
+	}
+
+	//Ask for HTML-friendly debug output
+	if($DEBUG)
+		$mail->Debugoutput = 'html';
+
+		//Set the hostname of the mail server
+		$mail->Host = 'smtp.gmail.com';
+		// use
+		// $mail->Host = gethostbyname('smtp.gmail.com');
+		// if your network does not support SMTP over IPv6
+
+		//Set the SMTP port number - 587 for authenticated TLS, a.k.a. RFC4409 SMTP submission
+		$mail->Port = 587;
+
+		//Set the encryption system to use - ssl (deprecated) or tls
+		$mail->SMTPSecure = 'tls';
+
+		//Whether to use SMTP authentication
+		$mail->SMTPAuth = true;
+
+		//Username to use for SMTP authentication - use full email address for gmail
+		$mail->Username = $EMAIL;
+
+		//Password to use for SMTP authentication
+		$mail->Password = $PASSWORD;
+
+		//Set who the message is to be sent from
+		$mail->setFrom($EMAIL, 'Holiday');
+
+		//Set an alternative reply-to address
+		$mail->addReplyTo($EMAIL, 'Holiday');
+
+		//Set who the message is to be sent to
+		$mail->addAddress($to, $from);
+
+		//Set the subject line
+		$mail->Subject = $subject;
+
+		//Read an HTML message body from an external file, convert referenced images to embedded,
+		//convert HTML into a basic plain-text alternative body
+		$mail->msgHTML($body);
+
+		//Replace the plain text body with one created manually
+		$mail->AltBody = $body;
+
+		//Attach an image file
+		//$mail->addAttachment('images/phpmailer_mini.png');
+
+		//send the message, check for errors
+		if (!$mail->send()) {
+			logEntry( "Mailer Error: " . $mail->ErrorInfo);
+		} else {
+			logEntry( "Message sent!");
+		}
+
+}
 
 //fork a non blocking fppd process
 
